1853de6e41da8dff85dfd006817df88efd3eb72b
Nessaa16 <vpriscilia@student.ciputra.ac.id>
2026-04-09 00:19:45 +0700

    refactor: adjust sitemap

--- frontend/src/app/sitemap.ts
+++ frontend/src/app/sitemap.ts
@@ -9,16 +9,34 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 1,
     },
     {
+      url: "https://sturdy.ie/about",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
       url: "https://sturdy.ie/case-studies",
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 0.8,
     },
     {
-      url: "https://sturdy.ie/insights",
+      url: "https://sturdy.ie/myinsights",
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 0.7,
+    },
+    {
+      url: "https://sturdy.ie/leadership-papers",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.7,
+    },
+    {
+      url: "https://sturdy.ie/contact",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.5,
     },
   ];
 }
